a887a9a594b4c3834c210bc54e9149885784f48e
Lakshman <Hanumandla14334@gmail.com>
2022-10-23 16:42:53 +0000

    commit

--- code.py
+++ code.py
@@ -1,15 +1,3 @@
 import numpy as np
-<<<<<<< HEAD
 import pandas as pd
 # this is analytics branch
-=======
-import pandas as pd 
-
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# Add to master
->>>>>>> 109082b6fcc51ded1068c6ae0c6e7fbb6b3c1a14
