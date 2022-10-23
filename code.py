72e282e19500f1f98bdae131675ddccf5f9594ee
Lakshman <Hanumandla14334@gmail.com>
2022-10-23 19:23:58 +0000

    line added

--- code.py
+++ code.py
@@ -1,24 +1,4 @@
 import numpy as np
 import pandas as pd
 
-<<<<<<<<< Temporary merge branch 1
-# This line is writen with dev branch
-# This line is writen with dev branch
-# This line is writen with dev branch
-# This line is writen with dev branch
-
-# This line is writen with dev branch
-# This line is writen with dev branch
-# This line is writen with dev branch
-
-# writing again
-#writing again
-=========
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# this line is added from master branch 
-# Add to master
->>>>>>>>> Temporary merge branch 2
+# This line is from analytics branch
