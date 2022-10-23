8d89744d8688b1bc23f35e0c5860a633481d3f72
Lakshman <Hanumandla14334@gmail.com>
2022-10-23 19:31:05 +0000

    no change

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-<<<<<<< HEAD
 # This line is writen with dev branch
 # This line is writen with dev branch
 # This line is writen with dev branch
@@ -10,8 +9,6 @@ import pandas as pd
 # This line is writen with dev branch
 # This line is writen with dev branch
 # This line is writen with dev branch
-=======
 #This line is written in feature branch
 #this second line is added from feature branch
 # line 3 in feature
->>>>>>> feature
